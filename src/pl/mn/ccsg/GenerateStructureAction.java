dbadfd87bb71e449b3643697316239e901dd471a
Lidjan <minidmnv@gmail.com>
2017-10-15 12:22:32 +0200

    Make action successfully visible and ready to action

--- src/pl/mn/ccsg/GenerateStructureAction.java
+++ src/pl/mn/ccsg/GenerateStructureAction.java
@@ -1,12 +1,13 @@
 package pl.mn.ccsg;
 
+import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAwareAction;
 
 /**
  * @author minidmnv
  */
-public class GenerateStructureAction extends DumbAwareAction {
+public class GenerateStructureAction extends AnAction {
 
     public GenerateStructureAction() {
         super("Generate Class Structure", "Easily generate class structure from active changelist", null);
@@ -14,7 +15,9 @@ public class GenerateStructureAction extends DumbAwareAction {
 
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
-
+        System.out.println("No i teraz bede generowal struktury klas");
     }
+
+
 
 }
