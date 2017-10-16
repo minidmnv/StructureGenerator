05223fa2393f0d9620ca036d18f7d72d9b2efaa6
Lidjan <minidmnv@gmail.com>
2017-10-16 18:53:57 +0200

    Add class structure generate action to ChangeList popup menu

--- src/pl/mn/ccsg/GenerateStructureAction.java
+++ src/pl/mn/ccsg/GenerateStructureAction.java
@@ -2,7 +2,13 @@ package pl.mn.ccsg;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.project.DumbAwareAction;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.changes.ChangeList;
+
+import java.util.Arrays;
+
+import static com.intellij.openapi.actionSystem.CommonDataKeys.PROJECT;
+import static com.intellij.openapi.vcs.VcsDataKeys.CHANGE_LISTS;
 
 /**
  * @author minidmnv
@@ -14,10 +20,13 @@ public class GenerateStructureAction extends AnAction {
     }
 
     @Override
-    public void actionPerformed(AnActionEvent anActionEvent) {
+    public void actionPerformed(AnActionEvent event) {
+        Project project = event.getRequiredData(PROJECT);
+        ChangeList[] changeLists = event.getRequiredData(CHANGE_LISTS);
+
         System.out.println("No i teraz bede generowal struktury klas");
+        System.out.println(project);
+        Arrays.stream(changeLists).forEach(ChangeList::getName);
     }
-
-
 
 }
