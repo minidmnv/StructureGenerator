1111a970790d507a6404cb4f906cf537bbe3c9bb
Lidjan <minidmnv@gmail.com>
2017-11-01 09:48:16 +0100

    Take module output path for every file from changelist to find class files to be copied.

--- src/pl/mn/ccsg/action/GenerateStructureAction.java
+++ src/pl/mn/ccsg/action/GenerateStructureAction.java
@@ -2,7 +2,13 @@ package pl.mn.ccsg.action;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.CompilerModuleExtension;
+import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.vcs.actions.VcsContextWrapper;
+import com.intellij.openapi.vcs.changes.Change;
+import com.intellij.openapi.vcs.changes.ChangeListManager;
+import com.intellij.openapi.vcs.changes.LocalChangeList;
 import pl.mn.ccsg.PathQualityAssurance;
 import pl.mn.ccsg.dialog.GenerateStructureDialog;
 
@@ -13,14 +19,17 @@ import static com.intellij.openapi.ui.DialogWrapper.OK_EXIT_CODE;
  */
 public class GenerateStructureAction extends AnAction {
 
+    private Project project;
+
     public GenerateStructureAction() {
         super("Generate Class Structure", "Easily generate class structure from active changelist", null);
     }
 
     @Override
     public void actionPerformed(AnActionEvent event) {
+        project = VcsContextWrapper.createCachedInstanceOn(event).getProject();
         GenerateStructureDialog dialog =
-                GenerateStructureDialog.instance(VcsContextWrapper.createCachedInstanceOn(event).getProject());
+                GenerateStructureDialog.instance(project);
         dialog.show();
 
         handleDialogAction(dialog);
@@ -36,8 +45,19 @@ public class GenerateStructureAction extends AnAction {
         }
     }
 
-    private void generateClassStructure(String patchName) {
-        //there is where magic happens
+    private void generateClassStructure(String patchDirectory) {
+        ChangeListManager changeListManager = ChangeListManager.getInstance(project);
+        LocalChangeList activeChangeList = changeListManager.getDefaultChangeList();
+
+        activeChangeList.getChanges()
+                .stream()
+                .filter(change -> !change.getType().equals(Change.Type.DELETED))
+                .map(Change::getVirtualFile)
+                .map(file -> ProjectFileIndex.SERVICE.getInstance(project).getModuleForFile(file, false))
+                .map(CompilerModuleExtension::getInstance)
+                .map(CompilerModuleExtension::getCompilerOutputPath)
+                .forEach(System.out::println);
+
     }
 
 
